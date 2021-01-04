8dc3facf39bc68ae455570ca93d63d70f18d076a
unknown <jeffpigg5@gmail.com>
2021-01-04 13:38:09 -0600

    Initial commit for the Custom List Project, I made a Test class and a List class so far to test methods before adding them to the Custom List offically

---
+++ AddTestMethods/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomListProject;
+
+namespace AddTestMethods
+{
+    [TestClass]
+    public class CustomListClassTests
+    {
+        [TestMethod]
+        public void AddOneItemToList_GetCount()
+        {
+            // Arrange
+            
+
+            // Act
+
+            // Assert
+        }
+
+        //[TestMethod]
+        //public void AddTwoItemsToList_GetCompacity()
+        //{
+
+        //}
+
+        //[TestMethod]
+        //public void AddTwoItemsToList_GetIndex()
+        //{
+
+        //}
+    }
+    
+}
