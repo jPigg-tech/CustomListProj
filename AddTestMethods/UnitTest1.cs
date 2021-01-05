5bd315c56ffc56abff7227a37cfd60caad898165
unknown <jeffpigg5@gmail.com>
2021-01-05 09:14:05 -0600

    Added second test method for the Add method in CustomList.cs

--- AddTestMethods/UnitTest1.cs
+++ AddTestMethods/UnitTest1.cs
@@ -24,17 +24,32 @@ namespace AddTestMethods
             Assert.AreEqual(expected, actual);
         }
 
-        //[TestMethod]
-        //public void AddTwoItemsToList_GetCompacity()
-        //{
+        //add multiple values, count increases
 
-        //}
+        [TestMethod]
+        public void AddMultipleVariablesToList_GetCount()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 3;
+            int actual;
+            int value1 = 5;
+            int value2 = 7;
+            int value3 = 10;
 
-        //[TestMethod]
-        //public void AddTwoItemsToList_GetIndex()
-        //{
+            // Act
+            testList.Add(value1);
+            testList.Add(value2);
+            testList.Add(value3);
+            actual = testList.Count;
 
-        //}
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        //create a list check capacity, should be 4
+        //create a list add 5 items, check capacity, should be 8
+        //add one value, check the first index of the list.  should be the value added
+
     }
     
 }
