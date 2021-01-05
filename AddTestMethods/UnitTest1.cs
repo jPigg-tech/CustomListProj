6c2c2b6756f55ef56486488124f452bf9e5f6212
unknown <jeffpigg5@gmail.com>
2021-01-05 09:31:39 -0600

    Created a testMethod to check the capacity after adding 5 itmes to UnitTest1.cs

--- AddTestMethods/UnitTest1.cs
+++ AddTestMethods/UnitTest1.cs
@@ -45,6 +45,7 @@ namespace AddTestMethods
             // Assert
             Assert.AreEqual(expected, actual);
         }
+
         //create a list check capacity, should be 4
         [TestMethod]
         public void CreateAListList_GetCapacity()
@@ -64,6 +65,31 @@ namespace AddTestMethods
         }
 
         //create a list add 5 items, check capacity, should be 8
+        [TestMethod]
+        public void CreateAListList_Add5Items_GetCapacity()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 8;
+            int actual;
+            int value1 = 5;
+            int value2 = 7;
+            int value3 = 10;
+            int value4 = 15;
+            int value5 = 23;
+
+
+            // Act
+            testList.Add(value1);
+            testList.Add(value2);
+            testList.Add(value3);
+            testList.Add(value4);
+            testList.Add(value5);
+            actual = testList.capacity;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
         //add one value, check the first index of the list.  should be the value added
 
     }
