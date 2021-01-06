fe6da13a934276861f072921e5817a91b97b753c
unknown <jeffpigg5@gmail.com>
2021-01-06 10:48:52 -0600

    Added a test method to test the count when Removing one value from the list in RemoveTests1.cs

--- RemoveTestMethods/RemoveTests1.cs
+++ RemoveTestMethods/RemoveTests1.cs
@@ -8,8 +8,32 @@ namespace RemoveTestMethods
     public class RemoveTests1
     {
         [TestMethod]
-        public void TestMethod1()
+
+        // Remove one value get count
+        public void CreateAListOfItems_RemoveOneItem_GetCount()
         {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 2;
+            int actual;
+            int value1 = 5;
+            int value2 = 10;
+            int value3 = 20;
+
+            // Act
+            testList.Add(value1);
+            testList.Add(value2);
+            testList.Add(value3);
+            testList.Remove(value3);
+            actual = testList.Count;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
+    }
+
+        // Remove multiple values, count decreases
+        // Create a list add 5 values, check capacity, should be 8
+        // Remove one value, check if index updated
     }
 }
