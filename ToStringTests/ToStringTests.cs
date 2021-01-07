5d8fa4999d91fba3685e10702557efe3a658c5f7
unknown <jeffpigg5@gmail.com>
2021-01-07 09:11:50 -0600

    Created a TestMethod in ToStringTests.cs to test that the method would change a single int added to a list in to a stirng

---
+++ ToStringTests/ToStringTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomListProject;
+
+namespace ToStringTests
+{
+    [TestClass]
+    public class ToStringTests
+    {
+        [TestMethod]
+        public void ReturnsOneIntInToAString()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            string expected = "1992";
+            int actual;
+            int value1 = 1992;
+
+
+            // Act
+
+            testList.Add(value1);
+            actual = testList.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        
+    }
+}
