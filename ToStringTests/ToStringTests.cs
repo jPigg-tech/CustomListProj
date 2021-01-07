eca32dcd7cce08e6a3d77864b6f87e8347f41aff
unknown <jeffpigg5@gmail.com>
2021-01-07 09:29:57 -0600

    Notices some reference type mistakes throughout the test code, corrected those, Also Added a test method in ToStringTests.cs to test if a object is added to a list can we turn it to a string

--- ToStringTests/ToStringTests.cs
+++ ToStringTests/ToStringTests.cs
@@ -13,7 +13,7 @@ namespace ToStringTests
             // Arrange
             CustomList<int> testList = new CustomList<int>();
             string expected = "1992";
-            int actual;
+            string actual;
             int value1 = 1992;
 
             // Act
@@ -31,7 +31,7 @@ namespace ToStringTests
             // Arrange
             CustomList<int> testList = new CustomList<int>();
             string expected = "2020, 2021";
-            int actual;
+            string actual;
             int value1 = 2020;
             int value2 = 2021;
 
@@ -51,17 +51,35 @@ namespace ToStringTests
             // Arrange
             CustomList<int> testList = new CustomList<int>();
             string expected = "Jeff";
-            int actual;
-            int value1 = 'J';
-            int value2 = 'e';
-            int value3 = 'f';
-            int value4 = 'f';
+            string actual;
+            char value1 = 'J';
+            char value2 = 'e';
+            char value3 = 'f';
+            char value4 = 'f';
 
             // Act
             testList.Add(value1);
             testList.Add(value2);
             testList.Add(value3);
             testList.Add(value4);
+            actual = testList.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Add an object to a list and return it as a string 
+        [TestMethod]
+        public void AddOneObjectToAList_ReturnItToAString()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            string expected = "pizza";
+            string actual;
+            T value1 = pizza;
+
+            // Act
+            testList.Add(value1);
             actual = testList.ToString();
 
             // Assert
