28fc484b9a8840a787618daa277f4db7a612ab58
unknown <jeffpigg5@gmail.com>
2021-01-07 09:13:30 -0600

    Created a TestMethod in ToStringTests.cs to test that the method would change multiple ints added to a list into strings

--- ToStringTests/ToStringTests.cs
+++ ToStringTests/ToStringTests.cs
@@ -26,6 +26,26 @@ namespace ToStringTests
             Assert.AreEqual(expected, actual);
         }
 
-        
+        // Add multiple ints to a list, return them as a string
+        [TestMethod]
+        public void ReturnsMultipleIntsInToAString()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            string expected = "2020, 2021";
+            int actual;
+            int value1 = 2020;
+            int value2 = 2021;
+
+
+            // Act
+
+            testList.Add(value1);
+            testList.Add(value2);
+            actual = testList.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
