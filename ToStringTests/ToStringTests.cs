9033a66b00efb6ebcb541b0ba7902533b4740489
unknown <jeffpigg5@gmail.com>
2021-01-07 09:17:59 -0600

    Created a TestMethod in ToStringTests.cs to test that the method would change multiple chars into one string

--- ToStringTests/ToStringTests.cs
+++ ToStringTests/ToStringTests.cs
@@ -16,9 +16,7 @@ namespace ToStringTests
             int actual;
             int value1 = 1992;
 
-
             // Act
-
             testList.Add(value1);
             actual = testList.ToString();
 
@@ -37,11 +35,33 @@ namespace ToStringTests
             int value1 = 2020;
             int value2 = 2021;
 
-
             // Act
-
             testList.Add(value1);
             testList.Add(value2);
+            actual = testList.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Add multiple chars to a list, return them as a string
+        [TestMethod]
+        public void AddMultipleCharsToAList_ReturnThemAsAString()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            string expected = "Jeff";
+            int actual;
+            int value1 = 'J';
+            int value2 = 'e';
+            int value3 = 'f';
+            int value4 = 'f';
+
+            // Act
+            testList.Add(value1);
+            testList.Add(value2);
+            testList.Add(value3);
+            testList.Add(value4);
             actual = testList.ToString();
 
             // Assert
