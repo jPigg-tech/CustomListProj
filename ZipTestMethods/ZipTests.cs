095d7312ed826b497f437d99c1eb54e2de42b505
unknown <jeffpigg5@gmail.com>
2021-01-13 06:56:01 -0600

    Created a Test Method in ZipTests.cs to test the zip ability whne i have an odd list and an even list

--- ZipTestMethods/ZipTests.cs
+++ ZipTestMethods/ZipTests.cs
@@ -8,8 +8,35 @@ namespace ZipTestMethods
     public class ZipTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ZipAbility_CombineTwoLists_OddsAndEvens()
         {
+            // Arrange
+            CustomList<int> odd = new CustomList<int>();
+            CustomList<int> even = new CustomList<int>();
+
+            CustomList<int> expected = new CustomList<int>();
+            expected.Add(1);
+            expected.Add(2);
+            expected.Add(3);
+            expected.Add(4);
+            expected.Add(5);
+            expected.Add(6);
+
+            CustomList<int> actual = new CustomList<int>();
+
+            // Act
+            odd.Add(1);
+            odd.Add(3);
+            odd.Add(5);
+
+            even.Add(2);
+            even.Add(4);
+            even.Add(6);
+
+            actual = odd.Zip(even);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
