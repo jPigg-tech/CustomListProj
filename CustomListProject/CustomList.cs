ad4d15df6c6babd34cc731ee997bcb16ab0e8158
unknown <jeffpigg5@gmail.com>
2021-01-14 14:55:45 -0600

    Added a Bubble sort method to CustomList.cs, added an exchange method too, to make things run smoother.

--- CustomListProject/CustomList.cs
+++ CustomListProject/CustomList.cs
@@ -256,5 +256,27 @@ namespace CustomListProject
             }
             return index;
         }
+        public static void exchange(int[] data, int m, int n)
+        {
+            int temporary;
+
+            temporary = data[m];
+            data[m] = data[n];
+            data[n] = temporary;
+        }
+        public static void IntArrayBubbleSort(int[] data)
+        {
+            int i, j;
+            int N = data.Length;
+
+            for (j = N - 1; j > 0; j--)
+            {
+                for (i = 0; i < j; i++)
+                {
+                    if (data[i] > data[i + 1])
+                        exchange(data, i, i + 1);
+                }
+            }
+        }
     }    
 }
